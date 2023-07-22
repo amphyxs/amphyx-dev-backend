7e52b3598b0bc61cc77bc68cb6cb3839794068bc
amphyxs <amphyxs@gmail.com>
2023-07-22 21:53:07 +0300

    Configured allowed hosts

--- amphyxdev/settings.py
+++ amphyxdev/settings.py
@@ -14,7 +14,11 @@ SECRET_KEY = environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'amphyx.online',
+    'localhost',
+    '127.0.0.1',
+]
 
 
 # Application definition
