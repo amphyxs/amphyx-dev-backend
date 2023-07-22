f8c9e7770c8310766d0b2d50a25cc498c5ff36aa
amphyxs <amphyxs@gmail.com>
2023-07-22 21:14:06 +0300

    Test for POST

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,8 +1,13 @@
 from rest_framework.response import Response
-from rest_framework.generics import RetrieveAPIView
+from rest_framework.generics import ListCreateAPIView
 from rest_framework import status
 
 
-class TestView(RetrieveAPIView):
-    def retrieve(self, request, *args, **kwargs):
+class TestView(ListCreateAPIView):
+
+    def post(self, request, *args, **kwargs):
+        print(self.request.body)
+        return Response('WTF', status=status.HTTP_200_OK)
+
+    def get(self, request, *args, **kwargs):
         return Response('Works', status=status.HTTP_200_OK)
