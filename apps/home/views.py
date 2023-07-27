8851f58a96b16296ee8b331a6f71afa832f6ae86
amphyxs <amphyxs@gmail.com>
2023-07-27 22:29:17 +0300

    :construction: [12] Test for building

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,5 +1,3 @@
-import PIL
-
 from rest_framework.response import Response
 from rest_framework.generics import ListCreateAPIView
 from rest_framework import status
@@ -9,7 +7,7 @@ class TestView(ListCreateAPIView):
 
     def post(self, request, *args, **kwargs):
         print(self.request.body)
-        response_text = PIL.__version__
+        response_text = 'Ohio'
         return Response(response_text, status=status.HTTP_200_OK)
 
     def get(self, request, *args, **kwargs):
