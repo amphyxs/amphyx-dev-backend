a50236ede65811d61c8b4fff73b724a8a2127feb
amphyxs <amphyxs@gmail.com>
2023-07-27 22:25:15 +0300

    :construction: [12] Added git reset in build script

--- deploy/build-production.sh
+++ deploy/build-production.sh
@@ -1,6 +1,7 @@
 cd ~/amphyx-dev-backend/
 git checkout main
 git pull
+git reset --hard origin/main
 ~/.local/share/pypoetry/venv/bin/poetry install
 ~/.local/share/pypoetry/venv/bin/poetry run python --version
 ~/.local/share/pypoetry/venv/bin/poetry run python ~/amphyx-dev-backend/manage.py migrate
