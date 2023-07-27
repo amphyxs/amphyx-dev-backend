3031346cebb0784ff516d9a2659379b08bfba335
amphyxs <amphyxs@gmail.com>
2023-07-27 21:53:54 +0300

    :construction: [12] Fixes in script

--- deploy/build-production.sh
+++ deploy/build-production.sh
@@ -2,6 +2,6 @@ cd ~/amphyx-dev-backend/
 git checkout main
 git pull
 ~/.local/share/pypoetry/venv/bin/poetry install
-~/.local/share/pypoetry/venv/bin/poetry shell
-python manage.py migrate
+~/.local/share/pypoetry/venv/bin/poetry run python --version
+~/.local/share/pypoetry/venv/bin/poetry run python ~/amphyx-dev-backend/manage.py migrate
 echo ✅ Done.
