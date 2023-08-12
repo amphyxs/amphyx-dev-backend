851c0166d2cad339802bf9021e8ddc0832f9e656
amphyxs <amphyxs@gmail.com>
2023-08-12 19:06:08 +0300

    :busts_in_silhouette: [17] Права доступа для эндпоинта

--- apps/personal_info/views.py
+++ apps/personal_info/views.py
@@ -1,11 +1,14 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAdminUser
+from apps.common.permissions import IsSuperuser, ReadOnly
 
 from apps.personal_info.models import ContactLink
 from apps.personal_info.serializers import ContactLinkSerializer
 
 
 class ContactLinkViewSet(ModelViewSet):
-    """Viewset для просмотра и редактирования списка ссылок-контактов."""
+    """ViewSet для просмотра и редактирования списка ссылок-контактов."""
 
+    permission_classes = [IsSuperuser | IsAdminUser | ReadOnly]
     queryset = ContactLink.objects.all()
     serializer_class = ContactLinkSerializer
