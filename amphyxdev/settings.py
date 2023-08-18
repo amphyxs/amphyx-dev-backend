19a7ca8563ea50f28c00c80b5fac86f5eb21ece8
amphyxs <amphyxs@gmail.com>
2023-08-18 18:19:53 +0300

    STATIC_URL вынесен в .env

--- amphyxdev/settings.py
+++ amphyxdev/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = environ.get('STATIC_URL', 'static/')
 
 STATIC_ROOT = environ.get('STATIC_ROOT', 'static/')
 
