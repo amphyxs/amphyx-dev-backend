577c4bb07f79e07f0385d5c6218c2a4783d0983f
amphyxs <amphyxs@gmail.com>
2023-08-18 18:17:27 +0300

    STATIC_ROOT вынесен в .env

--- amphyxdev/settings.py
+++ amphyxdev/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = 'static/'
+STATIC_ROOT = environ.get('STATIC_ROOT', 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
