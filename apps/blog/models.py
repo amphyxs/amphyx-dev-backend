87ffc35751415e70b66dc361e27d00fd85f20626
amphyxs <amphyxs@gmail.com>
2023-08-19 15:09:05 +0300

    :floppy_disk: [33] Модель BlogPost и миграция

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class BlogPost(models.Model):
+    """Пост в блоге."""
+
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
