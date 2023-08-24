f1fc857bce25f74934304db18e0a9020dc7f18ee
amphyxs <amphyxs@gmail.com>
2023-08-24 21:35:21 +0300

    :gear: [33] Форматирование дат в BlogPostShortSerializer

--- apps/blog/serializers.py
+++ apps/blog/serializers.py
@@ -1,10 +1,13 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 
 from apps.blog.models import BlogPost
 
 
-class BlogPostShortSerializer(ModelSerializer):
+class BlogPostShortSerializer(serializers.ModelSerializer):
     """Сериализатор постов блога для отображения их в списке."""
+
+    created_at = serializers.DateTimeField(format='%d.%m.%Y %H:%M')
+    updated_at = serializers.DateTimeField(format='%d.%m.%Y %H:%M')
 
     class Meta:
         fields = (
@@ -17,8 +20,11 @@ class BlogPostShortSerializer(ModelSerializer):
         model = BlogPost
 
 
-class BlogPostSerializer(ModelSerializer):
+class BlogPostSerializer(serializers.ModelSerializer):
     """Сериализатор постов блога с полным набором полей."""
+
+    created_at = serializers.DateTimeField(format='%d.%m.%Y %H:%M')
+    updated_at = serializers.DateTimeField(format='%d.%m.%Y %H:%M')
 
     class Meta:
         fields = '__all__'
